9e831b56e6ff4ee88e803f490bb904cab05431e8
Shad <shad@localhost.localdomain>
2023-03-18 11:46:57 -0400

    Make pymodule

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::{fs::{self, File}, path::Path, io::Write};
 
 use directories::BaseDirs;
+use pyo3::prelude::*;
 use serde::{Deserialize, Serialize};
 
-fn main() {
+#[pyfunction]
+fn setup() -> Vec<Task> {
     if let Some(base_dirs) = BaseDirs::new() {
         let data_dir = base_dirs.data_dir();
         let data = data_dir.to_str().unwrap().to_owned() + "/benri/";
@@ -24,17 +26,24 @@ fn main() {
                 let mut file = File::create(Path::new(&(main_folder + "Tasks.json"))).unwrap();
                 file.write_all(&contents).unwrap();
                 file.sync_all().unwrap();
-                println!("{:?}", vec![Task::default()]);
-                return;
+                return vec![Task::default()];
             },
         };
-        println!("{:?}", serde_json::from_str::<Vec<Task>>(&fs::read_to_string(main_file).unwrap()).unwrap());
+        return serde_json::from_str::<Vec<Task>>(&fs::read_to_string(main_file).unwrap()).unwrap();
     }
+    return vec![Task::default()];
 }
 
 #[derive(Default, Deserialize, Serialize, Debug)]
+#[pyclass]
 struct Task {
     name: String,
     completed: bool,
     link: Option<String>,
+}
+
+#[pymodule]
+fn benri_api(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(setup, m)?)?;
+    Ok(())
 }
