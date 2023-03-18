d2ff69037ce044e03789843362cd58bce91769ae
Aaron James <jamesa@bxscience.edu>
2023-03-18 15:18:01 -0400

    pain

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QLineEdit, QPushButton, Q
 from PyQt5.QtGui import * 
 from PyQt5.QtCore import * 
 import sys
-<<<<<<< HEAD
-from benri_api import *
+import time
+# from benri_api import *
 
-task = setup()
-print(task[0].name)
+# task = setup()
+# print(task[0].name)
 
-=======
-currentTask = None
->>>>>>> 9d39f32b840bb3b05aca8893cb6f73d2038b5cfb
 taskList = []
 
 app = QApplication(sys.argv)
@@ -20,10 +17,10 @@ mainWindow = QMainWindow()
 widget = QDockWidget()
 # taskListWidget = QDockWidget(mainWindow)
 
-addBreak = QPushButton("Add Break", widget)
-addBreak.move(100, 50)
-addBreak.resize(200, 40)
-addBreak.font().setPointSize(20)
+# addBreak = QPushButton("Add Break", widget)
+# addBreak.move(100, 50)
+# addBreak.resize(200, 40)
+# addBreak.font().setPointSize(20)
 
 
 taskEntry = QLineEdit(widget)
@@ -41,6 +38,20 @@ createTask.resize(200, 40)
 createTask.font().setPointSize(20)
 # createTask.setStyleSheet("background-color: #000000; color: #ffffff;")
 
+breakEntry = QLineEdit(widget)
+breakEntry.move(100, 980)
+breakEntry.resize(200, 40)
+
+createBreak = QPushButton("Create Break", widget)
+createBreak.move(300, 980)
+createBreak.resize(200, 40)
+createBreak.clicked.connect(lambda: createBreak())
+
+def createBreak():
+    newBreak = Break(breakEntry.text())
+    taskList.append(newBreak)
+    breakEntry.setText("")
+    
 
 
 def newTask():
@@ -65,6 +76,7 @@ class Task():
         self.tasklabel.setFont(QFont('Arial', 20))
         self.id = Task.numTasks
         Task.numTasks += 1
+        print(self.name)
         print(self.id)
         self.tasklabel.move(100, 140 + (self.id * 40))
         self.tasklabel.resize(400, 40)
@@ -110,23 +122,53 @@ class Task():
             return self.status
         def getName(self):
             return self.name
+        def setName(self, newName):
+            self.name = newName
+            self.tasklabel.setText(self.name)
+        def getId(self):
+            return self.id
+            
         
     
->>>>>>> 9d39f32b840bb3b05aca8893cb6f73d2038b5cfb
+# >>>>>>> 9d39f32b840bb3b05aca8893cb6f73d2038b5cfb
 
     
 
-# class Break(Task):
-#     super().__init__(self, "Break")
-#     self.time = 300
+class Break(Task):
+    if (isinstance(time, str)):
+        time = int(time)
+    else:
+        time = 10
+    time = 300
+    def __init__(self, time):
+        print("test1")
+        self.maxTime = int(time)
+        self.time = int(time)
+        super().__init__("break for: " + time)
+        self.checkBox.stateChanged.connect(lambda: self.start())
 
-#     while (self.checkbox.isChecked()):
-#         self.time -= 1
-#         time.sleep(1)
-#         if (self.time == 0):
-#             self.checkbox.setChecked(False)
-#             self.time = 300
-
+        
+    def start(self):
+        mainWindow.setWindowTitle("Break for: " + str(self.time))
+        mainWindow.setGeometry(0,0,25,25)
+        while (self.checkBox.isChecked()):
+            self.time -= 1
+            time.sleep(1)
+            if (self.time == 0):
+                self.checkBox.setChecked(False)
+                self.time = self.maxTime
+                break
+            if (self.checkBox.isChecked() == False):
+                self.time = self.maxTime
+                break
+            # setName("break for: " + str(self.time))
+        self.delete()
+        mainWindow.setWindowTitle("Benri")
+        mainWindow.setGeometry(100, 100, 500, 1080)
+        mainWindow.move(QDesktopWidget().availableGeometry().topLeft() - mainWindow.frameGeometry().topLeft())
+        # mainWindow.setWindowFlags(Qt.WindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint))
+            
+        
 
     
 
@@ -144,7 +186,7 @@ mainWindow.addDockWidget(Qt.LeftDockWidgetArea, widget)
 
 # mainWindow.setWindowFlags(Qt.WindowCloseButtonHint)
 mainWindow.move(QDesktopWidget().availableGeometry().topLeft() - mainWindow.frameGeometry().topLeft())
-mainWindow.setWindowFlags(Qt.WindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint))
+mainWindow.setWindowFlags(Qt.WindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.WindowCloseButtonHint | Qt.WindowMinimizeButtonHint ))
 
 
 # mainWindow.move()
